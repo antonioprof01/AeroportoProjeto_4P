e47a71f61245e9405595f985c28a87036a16ac4d
WILLDEGLAN GOMES DA SILVA <00257309322@ifto.local>
2022-06-15 21:35:04 -0300

    criacao de função super em clienteplus

--- AeroportoProjeto/src/ClientePlus.java
+++ AeroportoProjeto/src/ClientePlus.java
@@ -1,4 +1,9 @@
 public class ClientePlus extends Cliente {
+    public ClientePlus(int numPassagem, float numAssento, float preco_passagem) {
+        super(numPassagem, numAssento, preco_passagem);
+        //TODO Auto-generated constructor stub
+    }
+
     private int codig_plus;
 
     
