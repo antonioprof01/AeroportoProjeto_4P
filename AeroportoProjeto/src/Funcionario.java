40a251330f2d64d5375264b3150b6b4b0c871caf
unknown <helayne&ferreira@gmail.com>
2022-06-15 21:06:45 -0300

    Criaçao da classe funcionario

---
+++ AeroportoProjeto/src/Funcionario.java
@@ -0,0 +1,16 @@
+public abstract class Funcionario extends Pessoa {
+    
+    private int Cadastro;
+
+    public int getCadastro() {
+        return this.Cadastro;
+    }
+
+    public void setCadastro(int Cadastro) {
+        this.Cadastro = Cadastro;
+    }
+
+    public abstract void Registrar_Ponto();
+
+
+}
