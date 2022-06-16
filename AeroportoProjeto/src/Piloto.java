81544164566fc6d6078fb71c44b19e3dbc2cbe9a
Divina <marydivinasilva222@gmail.com>
2022-06-15 21:40:48 -0300

    alteração e implementação de metodos na classe Piloto

--- AeroportoProjeto/src/Piloto.java
+++ AeroportoProjeto/src/Piloto.java
@@ -9,10 +9,23 @@ public class Piloto extends Funcionario implements ComprarPassagem{
         this.numero_registro = numero_registro;
     }
     public void iniciarVoo(){
-        System.out.println("Iniciando o vooo:");
+        System.out.println("Iniciando o vooo, caros passageiros por favor desligue seus aparelhos celular e apertem o cinto e Boa Viagem !!");
     }
     public void registrarVooCompleto(){
-        System.out.println("Iniciando o vooo completo:");
+        System.out.println(" Vooo completoooo:");
+    }
+    public void cadastrarPassageiro(){
+        System.out.println(" Cadastrando o passageiro:");
+    }
+
+    public void gerarNumeroReserva(){
+        System.out.println(" Imforme seu numero reserva:");
+
+    }
+
+    public void gerarComprovante(){
+        System.out.println(" Comprovante:");
+
     }
 
 
